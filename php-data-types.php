066a618d3130293a4b6ca914b05081e39858573b
Nathan Rossow <nathanrossow@gmail.com>
2026-08-08 18:28:19 -0500

    Create php-data-types.php

---
+++ php-data-types.php
@@ -0,0 +1,6 @@
+$name = "Nathan";               // String
+$age = 35;                      // Integer
+$price = 19.99;                 // Float
+$isPublished = true;            // Boolean
+$nothing = null;                // Null
+$tags = ["PHP", "HTMX", "Web"]; // Array
